c1255617bd7845c7cf48369c4a7e8d33d42fbcb6
Luke Yang <cachehit99@gmail.com>
2023-11-28 21:41:06 +0900

    initial commit

---
+++ HCardParser.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime
+
+
+class HCardParser:
+    cols_to_drop = ["이용카드", "할부/회차", "예상적립/할인율(%)", "예상적립/할인", "결제후잔액", "수수료(이자)"]
+    col_map = {"이용일": "Date", "이용가맹점": "Place", "이용금액": "Price", "결제원금": "Principal"}
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def parse(self):
+        data = pd.read_html(self.file_name)[0]
+
+        # 컬럼이 멀티인덱스로 나오기 때문에 불필요한 1,2번째 인덱스를 제거
+        data.columns = data.columns.droplevel([0, 1])
+
+        # 불필요한 컬럼 및 데이터가 없는 행 제거
+        data = data.drop(columns=self.cols_to_drop)
+        data = data.dropna()
+
+        # 모든 카드사 데이터의 열 이름을 통일
+        data = data.rename(columns=self.col_map)
+
+        # Date 열을 date 포맷으로 변경
+        data["Date"] = data["Date"].apply(lambda x: datetime.strptime(x, "%Y년 %m월 %d일").date())
+
+        return data
---
+++ main.py
@@ -0,0 +1,6 @@
+from HCardParser import HCardParser
+
+hcard_parser = HCardParser('hyundaicard_20231128.xls')
+hcard_data = hcard_parser.parse()
+
+print()
