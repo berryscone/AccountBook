5daaa3b190cd5ca7c664ed823cd8c35c29f74e52
Luke Yang <hongmin.yang@lginnotek.com>
2023-12-08 01:00:05 +0900

    현대카드 컬럼이름 변경건 적용

--- HCardParser.py
+++ HCardParser.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class HCardParser:
-    cols_to_drop = ["이용카드", "할부/회차", "예상적립/할인율(%)", "예상적립/할인", "결제후잔액", "수수료(이자)"]
+    cols_to_drop = ["이용카드", "할부/회차", "적립/할인율(%)", "예상적립/할인", "결제후잔액", "수수료(이자)"]
     col_map = {"이용일": "Date", "이용가맹점": "Place", "이용금액": "Price", "결제원금": "Principal"}
 
     def __init__(self, file_name):
@@ -21,6 +21,9 @@ class HCardParser:
 
         # 모든 카드사 데이터의 열 이름을 통일
         data = data.rename(columns=self.col_map)
+
+        # 금액이 0인 부분 제거 (할인, 합계 등)
+        data = data[data["Price"] != 0]
 
         # Date 열을 date 포맷으로 변경
         data["Date"] = data["Date"].apply(lambda x: datetime.strptime(x, "%Y년 %m월 %d일").date())
