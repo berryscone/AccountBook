b349a7b91f4f9a6e6799ec3cfe11edcce80cbff1
Luke Yang <hongmin.yang@lginnotek.com>
2023-12-08 01:00:56 +0900

    최종 정리된 데이터를 엑셀로 저장할 때 유니코드 한글 정규화 적용 - 국민카드 데이터의 경우 정규화가 다른지 초성 중성 종성이 분리되어 보이는 문제 발생

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ import pandas as pd
 
 data_list = []
 
-hcard_parser = HCardParser('hyundaicard_20231128.xls')
+hcard_parser = HCardParser('hyundaicard_20231203.xls')
 hcard_data = hcard_parser.parse()
 data_list.append(hcard_data)
+
+kbank_parser = KBankParser("카카오뱅크_거래내역_N8426013673_2023120300165808.xlsx")
+kbank_data = kbank_parser.parse()
+data_list.append(kbank_data)
 
 scard_parser = SCardParser('이용대금명세서(신용카드).xls')
 scard_data = scard_parser.parse()
 data_list.append(scard_data)
 
-kbank_parser = KBankParser("카카오뱅크_거래내역_N5838885598_2023100716381561.xlsx")
-kbank_data = kbank_parser.parse()
-data_list.append(kbank_data)
-
-kcard_parser = KCardParser("202311_usage.xlsx")
+kcard_parser = KCardParser("202312_usage.xlsx")
 kcard_data = kcard_parser.parse()
 data_list.append(kcard_data)
 
 data = pd.concat(data_list, ignore_index=True)
 data = data.sort_values(by="Date", ascending=True, ignore_index=True)
+data["Place"] = data["Place"].str.normalize("NFC")
+
+data.to_excel("11월.xlsx", header=True, index=False)
 
 print()
